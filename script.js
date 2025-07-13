596a06ca6d1709ee251022bee0815834df7fc21c
MallaBhavya21 <bhavyamalla21@gmail.com>
2025-07-13 10:34:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,8 @@ form.addEventListener("submit", async function (e) {
   };
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyHZlvFP7I97cgInD7wAalp2kL-0CXzjk_-sMc6TrvXlgvZJBPDVOg_XHwI-FeedTGvPA/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyHZlvFP7I97cgInD7wAalp2kL-0CXzjk_-sMc6TrvXlgvZJBPDVOg_XHwI-FeedTGvPA/exec
+", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
